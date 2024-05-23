eb12f96e8d8af3b94fc6d94865a75fc98414ed01
sofia-adam <sofiaadam916@gmail.com>
2024-05-23 11:57:33 +0530

    today

--- may23/may23/urls.py
+++ may23/may23/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from work1 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('w1/',views.input)
 ]
--- may23/work1/views.py
+++ may23/work1/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from work1.forms import *
 
 # Create your views here.
+def input(request):
+    obj=student()
+    return render (request,'input.html',{'sform':obj})
